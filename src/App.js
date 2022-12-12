ec54f9ba30dae0f8c13bc71770330e1b1f8429ba
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2022-12-12 11:53:26 -0300

    fixed css and addded terms

--- src/App.js
+++ src/App.js
@@ -1,22 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <h1>Termos e condições gerais de uso do aplicativo iTutor</h1>
+        <p className="App-link">
+          Os serviços do iTutor são criados para fins educacionais a partir de
+          um trabalho de conclusão de curso para o Centro Universitário do Pará
+          - CESUPA, feita por uma equipe de alunos, composta por Elielson
+          Fernando do Santos Barbosa, portador da matrícula de nº 19070500;
+          Lucas da Silva Freitas, portador da matrícula de nº 19070002; E Pedro
+          Augusto Pinto de Lima, portador da matrícula de nº 19070031, titulares
+          da propriedade intelectual sobre software, website, aplicativos,
+          conteúdos e demais ativos relacionados à plataforma iTutor. A
+          plataforma visa licenciar o uso de seu software, website, aplicativos
+          e demais ativos de propriedade intelectual, fornecendo ferramentas
+          para auxiliar e dinamizar o dia a dia dos seus usuários. A plataforma
+          caracteriza-se pela prestação do seguinte serviço: Definição de
+          métricas de análise de desempenho de redes de discussão e a criação de
+          uma aplicação que irá permitir o registro e visualização das
+          interações dessas redes. O presente Termo estabelece obrigações
+          contratadas de livre e espontânea vontade, por tempo indeterminado,
+          entre a plataforma e as pessoas físicas ou jurídicas, usuárias do
+          aplicativo. Ao utilizar a plataforma o usuário aceita integralmente as
+          presentes normas e compromete-se a observá-las, sob o risco de
+          aplicação das penalidade cabíveis. A aceitação do presente instrumento
+          é imprescindível para o acesso e para a utilização de quaisquer
+          serviços fornecidos pela empresa. Caso não concorde com as disposições
+          deste instrumento, o usuário não deve utilizá-los. Serão utilizadas
+          todas as soluções técnicas à disposição do responsável pela plataforma
+          para permitir o acesso ao serviço 24 (vinte e quatro) horas por dia, 7
+          (sete) dias por semana. No entanto, a navegação na plataforma ou em
+          alguma de suas páginas poderá ser interrompida, limitada ou suspensa
+          para atualizações, modificações ou qualquer ação necessária ao seu bom
+          funcionamento. O acesso às funcionalidades da plataforma exigirá a
+          realização de um cadastro prévio, no qual o usuário irá informar os
+          campos de nome, email, senha. No qual esses dados serão utilizados
+          para devido uso de todas as funcionalidades da plataforma. Ao se
+          cadastrar o usuário deverá informar dados completos, recentes e
+          válidos, sendo de sua exclusiva responsabilidade manter referidos
+          dados atualizados, bem como o usuário se compromete com a veracidade
+          dos dados fornecidos. O usuário se compromete a não informar seus
+          dados cadastrais e/ou de acesso à plataforma a terceiros,
+          responsabilizando-se integralmente pelo uso que deles seja feito.
+          Mediante a realização do cadastro o usuário declara e garante
+          expressamente ser plenamente capaz, podendo exercer e usufruir
+          livremente dos serviços e produtos. Após a confirmação do cadastro, o
+          usuário possuirá um login e uma senha pessoal, a qual assegura ao
+          usuário o acesso individual à mesma. Desta forma, compete ao usuário
+          exclusivamente a manutenção de referida senha de maneira confidencial
+          e segura, evitando o acesso indevido às informações pessoais. Toda e
+          qualquer atividade realizada com o uso da senha será de
+          responsabilidade do usuário, que deverá informar prontamente a
+          plataforma em caso de uso indevido da respectiva senha. Não será
+          permitido ceder, vender, alugar ou transferir, de qualquer forma, a
+          conta, que é pessoal e intransferível. Caberá ao usuário assegurar que
+          o seu equipamento seja compatível com as características técnicas que
+          viabilize a utilização da plataforma e dos serviços ou produtos. O
+          usuário, ao aceitar os Termos e Política de Privacidade, autoriza
+          expressamente a plataforma a coletar, usar, armazenar, tratar, ceder
+          ou utilizar as informações derivadas do uso dos serviços, do site e
+          quaisquer plataformas, incluindo todas as informações preenchidas pelo
+          usuário no momento em que realizar ou atualizar seu cadastro, além de
+          outras expressamente descritas na Política de Privacidade que deverá
+          ser autorizada pelo usuário. O presente Termo de Uso concede aos
+          usuários uma licença não exclusiva, não transferível e não
+          sublicenciável, para acessar e fazer uso da plataforma e dos serviços
+          e produtos por ela disponibilizados. A estrutura do site ou
+          aplicativo, as marcas, logotipos, nomes comerciais, layouts, gráficos
+          e design de interface, imagens, ilustrações, fotografias,
+          apresentações, vídeos, conteúdos escritos e de som e áudio, programas
+          de computador, banco de dados, arquivos de transmissão e quaisquer
+          outras informações e direitos de propriedade intelectual da razão
+          social ASSOCIAÇÃO CULTURAL E EDUCACIONAL DO PARÁ, observados os termos
+          da Lei da Propriedade Industrial (Lei nº 9.279/96), Lei de Direitos
+          Autorais (Lei nº 9.610/98) e Lei do Software (Lei nº 9.609/98), estão
+          devidamente reservados. Este Termos de Uso não cede ou transfere ao
+          usuário qualquer direito, de modo que o acesso não gera qualquer
+          direito de propriedade intelectual ao usuário, exceto pela licença
+          limitada ora concedida. O uso da plataforma pelo usuário é pessoal,
+          individual e intransferível, sendo vedado qualquer uso não autorizado,
+          comercial ou não-comercial. Tais usos consistirão em violação dos
+          direitos de propriedade intelectual da razão social ASSOCIAÇÃO
+          CULTURAL E EDUCACIONAL DO PARÁ, puníveis nos termos da legislação
+          aplicável. Além do presente Termo, o usuário deverá consentir com as
+          disposições contidas na respectiva Política de Privacidade a ser
+          apresentada a todos os interessados dentro da interface da plataforma.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
